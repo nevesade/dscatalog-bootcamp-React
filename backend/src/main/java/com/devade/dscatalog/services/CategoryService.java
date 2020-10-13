99fb7214cac827be80580e1a94a78ceaa4c0d477
nevesade <adenasiocarvalho19@gmail.com>
2020-10-13 20:01:24 +0100

    DTO

--- backend/src/main/java/com/devade/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devade/dscatalog/services/CategoryService.java
@@ -1,11 +1,14 @@
 package com.devade.dscatalog.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devade.dscatalog.dto.CategoryDTO;
 import com.devade.dscatalog.entities.Category;
 import com.devade.dscatalog.repositories.CategoryRepository;
 
@@ -16,8 +19,20 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true )
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		
+		return  list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toUnmodifiableList());
+		
+		
+		/*
+		List<CategoryDTO> listDto = new ArrayList<>();
+		for(Category cat : list) {
+			listDto.add(new CategoryDTO(cat));
+		} */
+		
+		
+		
 	}
 	
 
