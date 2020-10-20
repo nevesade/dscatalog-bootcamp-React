63e39396b3f7ce2442a32049b879b4b55a926b42
nevesade <adenasiocarvalho19@gmail.com>
2020-10-20 11:39:09 +0100

    Add front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,40 +1,11 @@
-import React, {useState, useEffect} from 'react';
+import React  from 'react';
 
 
 const App = ( ) =>{
 
-    const [counter, setCounter] = useState(0);
-
-   
-
-    useEffect(() => {
-        console.log(" o contador mudou de valor ")
-    }, [counter]);
-
     return (
-        <div className="container mt-5">
-          <button 
-            className="btn btn-primary mr-5"
-            onClick={() => setCounter(counter + 1)}
-             >
-              +
-          </button>
-
-          <span>
-              {counter}
-          </span>
-
-          <button 
-            className="btn btn-primary ml-5"
-            onClick={() =>setCounter(counter - 1)}
-            >
-              -
-          </button>
-         {counter > 5 &&  <h1>O valor é maior que 5</h1> }
-         {counter <= 5 &&  <h1>O valor é menor ou igual que 5</h1> }
-        </div>
-        
-    )
+        <h1>APP</h1>
+    );
 }
 
 
