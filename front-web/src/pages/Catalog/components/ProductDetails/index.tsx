4a32468808809a62fc58800c83a6c9ba0ef2be1b
nevesade <adenasiocarvalho19@gmail.com>
2020-10-21 20:57:54 +0100

    Show details products voie API

--- front-web/src/pages/Catalog/components/ProductDetails/index.tsx
+++ front-web/src/pages/Catalog/components/ProductDetails/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import './styles.scss';
 import { ReactComponent as ArrowIcon } from '../../../../core/assets/images/arrow.svg';
-import { ReactComponent as ProductImage } from '../../../../core/assets/images/product.svg';
 import ProductPrice from '../../../../core/assets/styles/components/ProductPrice';
+import { makeRequest } from '../../../../core/utilis/request';
+import { Product } from '../../../../core/types/Products';
 
 type ParamsType = {
     productId: string;
@@ -13,7 +14,16 @@ type ParamsType = {
 const ProductDetails = () =>{
 
     const { productId } = useParams<ParamsType>();
+    const [product, setProduct] = useState<Product>();
     
+
+
+    useEffect(() => {
+
+        makeRequest( {url: `/products/${productId}`})
+        .then(response => setProduct(response.data) )
+
+    }, [productId]);
 
     console.log(productId);
 
@@ -30,12 +40,12 @@ const ProductDetails = () =>{
                   <div className="row">
                       <div className="col-6 pr-5"> 
                         <div className="product-details-card text-center">
-                                <ProductImage className="product-details-image" />
+                                <img src={product?.imgUrl} alt={product?.name} className="product-details-image"/>
                         </div>
                         <h1 className="product-details-name">
-                            Computador Desktop - Intel Core i7
+                           {product?.name}
                         </h1>
-                        <ProductPrice price={37999}/>
+                            {product?.price && <ProductPrice price= {product?.price} /> }
                       </div>
                       <div className="col-6 product-details-card ">
                           <h1 className="product-description-title">
@@ -43,11 +53,7 @@ const ProductDetails = () =>{
                          </h1>
                          
                          <p className="product-description-text"> 
-                         Seja um mestre em multitarefas com a capacidade para exibir quatro
-                          aplicativos simultâneos na tela. A tela está ficando abarrotada?
-                           Crie áreas de trabalho virtuais para obter mais espaço e trabalhar
-                            com os itens que você deseja. Além disso, todas as notificações e 
-                            principais configurações são reunidas em uma única tela de fácil acesso.
+                         {product?.description}
 
                          </p>
                          
