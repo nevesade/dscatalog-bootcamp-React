bac67630a9dd02936444efdf13d387f25b1083f5
nevesade <adenasiocarvalho19@gmail.com>
2020-11-06 23:54:47 +0000

    Created Login Form

--- front-web/src/pages/Admin/Auth/components/Login/index.tsx
+++ front-web/src/pages/Admin/Auth/components/Login/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import ButtonIcon from '../../../../../core/assets/styles/components/ButtonIcon';
 import AuthCard from '../Card';
 import './styles.scss';
 
@@ -10,7 +12,31 @@ const Login = () => {
         
         <AuthCard title="login">
             <form className="login-form">
-                <h1>Formulário de Login</h1>
+                <input 
+                    type="email" 
+                    className="form-control input-base margin-bottom-30"
+                    placeholder="Email">
+                </input>
+                <input 
+                    type="password" 
+                    className="form-control input-base"
+                    placeholder="Senha">
+                </input>
+                <Link to="/admin/auth/recover" className="login-link-recover">
+                    Esqueci a senha?
+                </Link>
+                <div className="login-submit">
+                    <ButtonIcon text="LOGAR" />
+                </div>
+
+                <div className="text-center">
+                    <span className="not-registered">
+                        Não tem cadastro?
+                    </span>
+                    <Link to="/admin/auth/register" className="login-link-register">
+                        CADASTRAR
+                    </Link>
+                </div>
             </form>
         </AuthCard>
     )
