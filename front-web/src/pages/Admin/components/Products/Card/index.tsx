ef62e9ecd2dec38a1bb644d836a6efff0635cc54
nevesade <adenasiocarvalho19@gmail.com>
2020-11-29 22:54:07 +0000

    Criando layout da listagem de produtos para o ADMIN - parte 2

--- front-web/src/pages/Admin/components/Products/Card/index.tsx
+++ front-web/src/pages/Admin/components/Products/Card/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProductPrice from '../../../../../core/assets/styles/components/ProductPrice';
 import './styles.scss';
 
 const Card = () => {
@@ -7,23 +8,34 @@ const Card = () => {
         <div className="card-base product-card-admin  ">
             <div className="row">
                 <div className="col-2 text-center  border-right py-3">
-                    <img 
-                    src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"
-                     alt="Produto teste"
-                     className="product-card-image-admin"
-                    
+                    <img
+                        src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"
+                        alt="Produto teste"
+                        className="product-card-image-admin"
+
                     />
 
                 </div>
                 <div className="col-7  py-3">
                     <h3 className="product-card-name-admin">
                         Computador i7
-
                     </h3>
+                    <ProductPrice price={45.50} />
 
                 </div>
-                <div className="col-3 py-3">
-                    <h1>Ações</h1>
+                <div className="col-3 pt-3 pr-5">
+                    <button
+                        type="button"
+                        className="btn btn-outline-secondary btn-block border-radius-10 mb-3 btn-edit"
+                        >
+                        EDITAR
+                  </button>
+                    <button
+                        type="button"
+                        className="btn btn-outline-danger btn-block border-radius-10 mb-3"
+                        >
+                        EXCLUIR
+                  </button>
 
                 </div>
 
